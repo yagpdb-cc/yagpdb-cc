e8bcf7ca55ed5106a8fcaf2437eaf1403178ab55
Joe <ravenclawjoe101325@gmail.com>
2020-01-31 22:46:12 -0800

    feat: serverinfo CC

---
+++ info/server.cc.lua
@@ -0,0 +1,60 @@
+{{- /*
+	This command allows you to view information about the server.
+	Usage: `-serverinfo`. (Use `-server icon` to view the server icon).
+
+	Recommended trigger: Regex trigger with trigger `^-(server|guild)(-?info)?`
+*/ -}}
+
+{{ $icon := "" }}
+{{ $name := printf "%s (%d)" .Guild.Name .Guild.ID }}
+{{ if .Guild.Icon }}
+	{{ $icon = printf "https://cdn.discordapp.com/icons/%d/%s.webp" .Guild.ID .Guild.Icon }}
+{{ end }}
+{{ $owner := userArg .Guild.OwnerID }}
+{{ $levels := cslice
+	"None: Unrestricted"
+	"Low: Must have a verified email on their Discord account."
+	"Medium: Must also be a member of this server for longer than 10 minutes."
+	"(╯°□°）╯︵ ┻━┻: Must also be a member of this server for longer than 10 minutes."
+	"┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻: Must have a verified phone on their Discord account."
+}}
+{{ $afk := "n/a" }}
+{{ if .Guild.AfkChannelID }}
+	{{ $afk = printf "**Channel:** <#%d> (%d)\n**Timeout:** %s"
+		.Guild.AfkChannelID
+		.Guild.AfkChannelID
+		(humanizeDurationSeconds (toDuration (mult .Guild.AfkTimeout .TimeSecond)))
+	}}
+{{ end }}
+{{ $embedsEnabled := "No" }}
+{{ if .Guild.EmbedEnabled }} {{ $embedsEnabled = "Yes" }} {{ end }}
+
+{{ $infoEmbed := cembed
+	"author" (sdict "name" $name "icon_url" $icon)
+	"color" 14232643
+	"thumbnail" (sdict "url" $icon)
+	"fields" (cslice
+		(sdict "name" "❯ Verification Level" "value" (index $levels .Guild.VerificationLevel))
+		(sdict "name" "❯ Region" "value" .Guild.Region)
+		(sdict "name" "❯ Members" "value" (printf "**• Total:** %d Members\n**• Online:** %d Members" .Guild.MemberCount onlineCount))
+		(sdict "name" "❯ Roles" "value" (printf "**• Total:** %d\nUse `-listroles` to list all roles." (len .Guild.Roles)))
+		(sdict "name" "❯ Owner" "value" (printf "%s (%d)" $owner.String $owner.ID))
+		(sdict "name" "❯ AFK" "value" $afk)
+		(sdict "name" "❯ Embeds Enabled" "value" $embedsEnabled)
+	)
+}}
+
+{{ if .CmdArgs }}
+	{{ if eq (index .CmdArgs 0) "icon" }}
+		{{ sendMessage nil (cembed
+			"author" (sdict "name" $name "icon_url" $icon)
+			"title" "❯ Server Icon"
+			"color" 14232643
+			"image" (sdict "url" $icon)
+		) }}
+	{{ else }}
+		{{ sendMessage nil $infoEmbed }}
+	{{ end }}
+{{ else }}
+	{{ sendMessage nil $infoEmbed }}
+{{ end }}
