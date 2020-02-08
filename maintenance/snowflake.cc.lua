724dc6dc43a1ea3efa1ce4a3f282a3bb5594b912
Joe <ravenclawjoe101325@gmail.com>
2020-02-08 10:21:32 -0800

    refactor(snowflake): Make it into a oneliner

--- maintenance/snowflake.cc.lua
+++ maintenance/snowflake.cc.lua
@@ -1,6 +1,13 @@
-{{ $epoch := 1420070400000 }}
+{{- /*
+	This command calculates the timestamp of a Discord snowflake.
+
+	Recommended trigger: Command trigger with trigger `snowflake`.
+*/ -}}
+
 {{ $snowflake := (parseArgs 1 "**Syntax:** -snowflake <snowflake>" (carg "int" "snowflake")).Get 0 }}
-{{ $timestamp := add (toInt (div $snowflake 4194304)) $epoch }}
-{{ $generated := (newDate 1970 1 1 0 0 0).Add (toDuration (mult $timestamp 1000000) ) }}
-**❯ Timestamp:** `{{ $generated }}`
-**❯ Formatted:** {{ $generated.Format "Jan 02, 2006 3:04 AM" }}
+
+{{/* Logic: Divide snowflake by 2 to the 22nd, add it to Discord epoch, multiply by 1 million to convert to nano, convert to duration, add to Unix epoch */}}
+{{ $timestamp := div $snowflake 4194304 | add 1420070400000 | mult 1000000 | toDuration | (newDate 1970 1 1 0 0 0).Add }}
+
+**❯ Timestamp:** `{{ $timestamp }}`
+**❯ Formatted:** {{ $timestamp.Format "Jan 02, 2006 3:04 AM" }}
