aa00998be4cca807df215aaec8c2aa4894692cb7
Satty9361 <56998894+Satty9361@users.noreply.github.com>
2020-02-15 16:14:00 +0100

    giveaway Reaction Command

---
+++ Giveaway/Vanilla Version/giveaway_reaction.cc.lua
@@ -0,0 +1,39 @@
+{{/* Supporting Reaction Command for Giveaway Package.
+Recommended Trigger : Reaction Trigger ; Type : Added + Removed reactions */}}
+
+{{$giveawayEmoji := `🎉`}} {{/* set giveaway emoji to be used */}}
+
+{{/* Actual Code , do not touch unless you know what youre doing */}}
+
+{{/* defining some global variables */}}
+{{ $data := sdict}}
+
+{{/* if reaction emoji matches giveaway emoji and user reacting is not a bot , proceed */}}
+{{if  and (eq .Reaction.Emoji.Name $giveawayEmoji) (not .User.Bot )}}
+
+{{/* fetching active giveaways data */}}
+{{with ( dbGet 7777 "giveaway_active" ).Value}}{{$data = sdict .}}{{end}}
+{{$giveawayData := $data.Get (joinStr "" .Reaction.ChannelID .Reaction.MessageID)}}
+
+{{/* if current message is an active giveaway announcement message */}}
+{{if $giveawayData}}
+{{$giveawayData = sdict $giveawayData}}
+
+{{/* if reaction was added increase count by 1 and add user ID to ID list */}}
+{{if .ReactionAdded}}
+     {{$giveawayData.Set "listID" (joinStr "" $giveawayData.listID  .User.ID "," )}}
+     {{$giveawayData.Set "count" (add $giveawayData.count 1)}}
+
+{{/* if reaction was removed reduce count by 1 and remove user ID from ID list */}}
+{{else}}
+     {{$IDregex := joinStr  ""  .User.ID `,`  }}
+     {{$giveawayData.Set "listID" (reReplace $IDregex $giveawayData.listID "")}}
+     {{$giveawayData.Set "count" (add $giveawayData.count -1)}}          
+{{end}}
+
+{{/* update active giveaway database entry */}}
+     {{$data.Set  (joinStr ""  .Reaction.ChannelID .Reaction.MessageID) $giveawayData}}{{dbSet 7777 "giveaway_active" $data}}
+
+{{end}}
+
+{{end}}
