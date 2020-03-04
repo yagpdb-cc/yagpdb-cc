6ed8a40fa3c792b18a7dfd2a5e50d9fe31c7a4a5
GenryMg <58843937+GenryMg@users.noreply.github.com>
2020-03-04 12:16:00 +0800

    Update meme.cc.lua

--- fun/meme.cc.lua
+++ fun/meme.cc.lua
@@ -2,7 +2,7 @@
 	This command allows you to make a meme. Usage: `-meme <meme template> >top text> <bottom text> <link if template is custom>`.
 	You can provide a link for a custom meme, or use the existing templates: both, buzz, doge, joker and sad-biden.
 	Sometimes, the special characters will mess up the link, so it is recommended that you only use these special characters: " " "?" "%" "#" "/"
-	Recommend trigger: Command trigger with trigger `meme`.
+	Recommend trigger: Command trigger with trigger `meme`. If you have any questions, contact me on discord: GenryMg#0001
 */}}
 
 
