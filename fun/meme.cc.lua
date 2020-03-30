0ae2f335b1beb3c217bbaad7c939dc237051613d
Joe <ravenclawjoe101325@gmail.com>
2020-03-30 09:18:03 -0700

    style(meme): format properly

--- fun/meme.cc.lua
+++ fun/meme.cc.lua
@@ -7,31 +7,36 @@
 */}}
 
 
-{{$link := "https://memegen.link"}}
-{{$args := parseArgs 3 (`Usage: -meme <meme template> "top text" "bottom text"`)
-(carg "string" "User")
-(carg "string" "User")
-(carg "string" "User")
-(carg "string" "User")}}
-{{$replacers := cslice " " "?" "%" "#" "/" }}
-{{$replacements := cslice "_" "~q" "~p" "~h" "~s" }}
-{{$meme := $args.Get 0 }}
-{{$top := ($args.Get 1)}}
-{{$bottom := ($args.Get 2)}}
-{{ range $i, $ := $replacers }}
-{{ $meme = split $meme . | joinStr (index $replacements $i) }}
-{{ $top = split $top . | joinStr (index $replacements $i) }}
-{{ $bottom = split $bottom . | joinStr (index $replacements $i) }}
-{{ end }}
-{{if gt (len .Args) 4}}
-{{$cuslink := ($args.Get 3)}}
-{{$msglink := joinStr "/" $link $meme $top $bottom}}
-{{$msglink = joinStr "" $msglink ".jpg?alt="}}
-{{$msglink1 := joinStr "" $msglink $cuslink}}
-{{ sendMessage nil (cembed "image" (sdict "url" $msglink1))}}
+{{ $link := "https://memegen.link" }}
+{{ $args := parseArgs 3 (`Usage: -meme <meme template> "top text" "bottom text"`)
+	(carg "string" "template")
+	(carg "string" "top-text")
+	(carg "string" "bottom-text")
+	(carg "string" "custom-link")
+}}
+{{ $replacers := cslice " " "?" "%" "#" "/" }}
+{{ $replacements := cslice "_" "~q" "~p" "~h" "~s" }}
+{{ $meme := $args.Get 0 }}
+{{ $top := $args.Get 1 }}
+{{ $bottom := $args.Get 2 }}
+{{- range $i, $ := $replacers }}
+	{{ $meme = split $meme . | joinStr (index $replacements $i) }}
+	{{ $top = split $top . | joinStr (index $replacements $i) }}
+	{{ $bottom = split $bottom . | joinStr (index $replacements $i) }}
+{{- end }}
+{{if $args.IsSet 3 }}
+	{{ $cuslink := $args.Get 3 }}
+	{{ $msglink := joinStr "/" $link $meme $top $bottom }}
+	{{ $msglink = joinStr "" $msglink ".jpg?alt=" }}
+	{{ $msglink1 := joinStr "" $msglink $cuslink }}
+	{{ sendMessage nil (cembed "image" (sdict "url" $msglink1)) }}
 {{else}}
-{{$msglink := joinStr "/" $link $meme $top $bottom}}
-{{$msglink = joinStr "" $msglink ".jpg"}}
-{{$col := randInt 111111 999999}}
-{{sendMessage nil (cembed "title" "Here is your meme" "image" (sdict "url" $msglink) "color" $col) }}
+	{{ $msglink := joinStr "/" $link $meme $top $bottom }}
+	{{ $msglink = joinStr "" $msglink ".jpg" }}
+	{{ $col := randInt 111111 999999 }}
+	{{ sendMessage nil (cembed
+		"title" "Here is your meme"
+		"image" (sdict "url" $msglink)
+		"color" $col
+	) }}
 {{end}}
