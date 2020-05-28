87bb9d650bca6ee99e827bc4ca58a29731c9252d
Satty9361 <56998894+Satty9361@users.noreply.github.com>
2020-05-28 19:58:46 +0200

    uniformity

--- snippets/standardize.cc.lua
+++ snippets/standardize.cc.lua
@@ -4,15 +4,15 @@
         Example use: {{$globalSdict.Set "val" $value}}{{template "standardize" $globalSdict}}{{$value = $globalSdict.Get "res"}}
 */}}
 
-{{$globalSdict := sdict }}
+{{$globalSdict:= sdict }}
 {{- define "standardize"}}
-{{- $value:= (.Get "val")}}{{$t := printf "%T" $value}}{{$recursiveSdict := sdict}}{{$rangeVal := ""}}
+{{- $value:= .Get "val"}}{{$t:= printf "%T" $value}}{{$recursiveSdict:= sdict}}{{$rangeVal:= ""}}
     {{- if (eq  $t `map[string]interface {}`)}}{{$rangeVal = sdict $value}}
     {{- else if (eq $t `[]interface {}`)}}{{$rangeVal = cslice.AppendSlice $value}}
     {{- else if (in (cslice `templates.SDict` `templates.Slice`) $t)}}{{$rangeVal = $value}} 
     {{- end}}
     {{- if (print $rangeVal)}}
-        {{- range $k,$v := $rangeVal}}
+        {{- range $k,$v:= $rangeVal}}
             {{- if in (cslice `map[string]interface {}` `[]interface {}` `templates.SDict` `templates.Slice`) (printf "%T" $v)}}
                 {{- $recursiveSdict.Set "val" $v}}{{template "standardize" $recursiveSdict}}{{$rangeVal.Set $k ($recursiveSdict.Get "res")}}
             {{- end}}
@@ -20,5 +20,5 @@
     {{- else}}
         {{- $rangeVal = $value}}
     {{- end}}
-{{- (.Set "res" $rangeVal)}}
+{{- .Set "res" $rangeVal}}
 {{- end}}
