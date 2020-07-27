89a58cdc001e96894858776f3ac1d42c20f1ee38
Satty9361 <56998894+Satty9361@users.noreply.github.com>
2020-07-27 02:56:25 +0200

    Fix: Suggestion V2 : Submission_Channel -> Logging_Channel

--- suggestion/v2/suggestion.cc.lua
+++ suggestion/v2/suggestion.cc.lua
@@ -7,7 +7,7 @@
 
 {{/* CONFIGURATION VARIABLES START */}}
 {{$Suggestion_Channel := 356486960417734666}}
-{{$Submission_Channel := 525535451839463424}}
+{{$Logging_Channel := 525535451839463424}}
 {{$Implemented_Channel := 532918109036740608}}
 {{$Mod_Roles := cslice 384008687951282177 419970533069815808}} 
 {{$Cooldown := 600}} {{/* Can be set to 0 for no cooldown */}}
@@ -23,7 +23,7 @@
 {{else}}
 	{{range $Mod_Roles}}{{if in $.Member.Roles .}}{{$IS_Mod = true}}{{end}}{{end}}
 {{end}}
- 
+
 {{if reFind `(?i)\bsuggest(ion)?\b` .Cmd }}
 	{{$Syntax = print .Cmd " <Suggestion_Here>"}}
 	{{$col := 16777215}}{{$pos := 0}}{{$r := .Member.Roles}}{{range .Guild.Roles}}{{if and (in $r .ID) (.Color) (lt $pos .Position)}}{{$pos = .Position}}{{$col = .Color}}{{end}}{{end}}
@@ -41,7 +41,7 @@
 			}}
 			{{$ID := sendMessageRetID $Suggestion_Channel $embed}}
 			{{addMessageReactions $Suggestion_Channel $ID "upvote:524907425531428864" "downvote:524907425032175638"}}
-			{{sendDM "Suggestion submitted successfully. If you want to discuss this or other suggestions, use the <#" $Submission_Channel "> channel. If you want to delete your suggestion, do so with `" $Prefix "deletesuggestion " $ID "` on the YAGPDB server."}}
+			{{sendDM "Suggestion submitted successfully. If you want to discuss this or other suggestions, use the <#" $Logging_Channel "> channel. If you want to delete your suggestion, do so with `" $Prefix "deletesuggestion " $ID "` on the YAGPDB server."}}
 			{{addReactions "upvote:524907425531428864"}}
 		{{end}}
 	{{else}}
@@ -50,7 +50,7 @@
 {{else}}
 	{{$authorID := 0}}{{$message := .nil}}{{$rest := ""}}{{$command := ""}}
 	{{$Syntax = print .Cmd " <Suggestion_ID> <Message/Arguments>"}}
- 
+
 	{{/* Suggestion Message Validation */}}
 	{{with reFindAllSubmatches (print `(?i)\A(?:` (reReplace `[\.\[\]\-\?\!\\\*\{\}\(\)\|]` $Prefix `\${0}`) `\s?|\S+\s*)(?:(del)\w+|(edit)\w+|\w+\s+(\w+))\s+(\d+)\s*(.*)`) .Message.Content}}
 		{{$command = lower (or (index . 0 1) (index . 0 2) (index . 0 3))}}
@@ -76,7 +76,7 @@
 	{{else}}
 		{{$error = "Invalid Syntax: did not provide valid Message ID"}}
 	{{end}}
- 
+
 	{{if not $error}}
 		{{$embed := structToSdict (index $message.Embeds 0)}}{{range $k,$v:=$embed}}{{if eq (kindOf $v true) `struct`}}{{$embed.Set $k (structToSdict $v)}}{{end}}{{end}}{{$embed.Author.Set "Icon_URL" $embed.Author.IconURL}}
 		{{if eq $command "del"}}
@@ -104,7 +104,7 @@
 						{{if getMessage $Suggestion_Channel .}}
 							{{$embed.Set "Description" (print $embed.Description "\n\n**This message has been marked as a dupe of:\n**https://discordapp.com/channels/" $.Guild.ID "/" $Suggestion_Channel "/" .)}}
 							{{deleteMessage $Suggestion_Channel $message.ID 0}}
-							{{sendMessage $Submission_Channel (complexMessage "content" (print "<@" $authorID "> | The suggestion below has been marked as dupe!") "embed" $embed)}}
+							{{sendMessage $Logging_Channel (complexMessage "content" (print "<@" $authorID "> | The suggestion below has been marked as dupe!") "embed" $embed)}}
 						{{else}}
 							{{$error = print "Invalid Original Suggestion Message ID : `" $rest "`"}}
 						{{end}}
@@ -116,7 +116,7 @@
 				{{end}}
 			{{else if eq $command "deny"}}
 				{{deleteMessage $Suggestion_Channel $message.ID 0}}
-				{{sendMessage $Submission_Channel (complexMessage "content" (print "<@" $authorID "> | The suggestion below has been deleted for reason: " $rest) "embed" $embed)}}
+				{{sendMessage $Logging_Channel (complexMessage "content" (print "<@" $authorID "> | The suggestion below has been deleted for reason: " $rest) "embed" $embed)}}
 			{{else}}
 				{{if $rest}}{{$embed.Set "Description" (print $embed.Description "\n\n**__Comment__**```" $rest "```")}}{{end}}
 				{{$embed.Set "Title" "Successfully Implemented Suggestion:"}}
@@ -129,7 +129,7 @@
 		{{end}}
 	{{end}}
 {{end}}
- 
+
 {{deleteTrigger 20}}{{deleteResponse 5}}
 {{if $error}}
 	{{$ID := sendMessageRetID nil (cembed "title" "Error" "color" 0xFF0000 "description" (print "**Error:** " $error "\n\n**Syntax:** `" $Syntax "`"))}}
