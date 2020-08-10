04d867b78da1442d61392d655b76103a712ca97e
Gas_Infinity <58077502+GasInfinity@users.noreply.github.com>
2020-08-10 09:49:01 +0200

    Now Supports Currency Emoji

--- birthday/birthday.cc.lua
+++ birthday/birthday.cc.lua
@@ -1,16 +1,17 @@
 {{/* User Variables */}}
 {{$mods := cslice 681482833944117250 740593852833857557}}
-{{$channelID := 740588980919205999}} {{/* Channel ID to send the bday msgs */}}
-{{$bdayMsg := "Congratulations for your birthday!"}}
+{{$channelID := 740590114861744250}} {{/* Channel ID to send the bday msgs */}}
+{{$bdayMsg := "**Congratulations for your birthday!**"}}
 {{$invertedOrder := false}}
 {{$kickUnderAge := false}}
 {{$banUnderAge := false}}
-{{$moneytogiveonbday := 100}} {{/* Money To Give On Birthdays */}}
-{{$moneyMsg := "Congratulations, You Gained "}} {{/* Money Msg To Send */}}
+
+{{$moneytogiveonbday := 1000}} {{/* Money To Give On Birthdays */}}
+{{$currency := "💰"}}
+{{$moneyMsg := "**Congratulations, You Gained "}}
 {{$moneyonbday := false}} {{/* Give Money OnBirthday? */}}
 {{ $dbHandName := "HAND" }} {{/* Database Entry To Put Currency, The entries are: UserID+$dbHandName */}}
 {{/* End */}}
-
 
 {{/* DONT TOUCH */}}
 {{/* Vars */}}
@@ -75,7 +76,7 @@
 			{{$send = true}}
 		{{end}}
 	{{end}}
-	{{if and $send (not (dbGet currentTime.Day "bdayannounced"))}} {{dbSet currentTime.Day "bdayannounced" true}} {{if $moneyonbday}}{{$moneyMsg = print $moneyMsg $moneytogiveonbday " For Your Birthday"}}{{ $increasemoney := dbIncr $user.ID $dbHandName $moneytogiveonbday }} {{sendMessageNoEscape nil $moneyMsg}} {{end}} {{sendMessageNoEscape nil $bdayMsg}} {{end}}
+	{{if and $send (not (dbGet currentTime.Day "bdayannounced"))}} {{dbSet currentTime.Day "bdayannounced" true}} {{if $moneyonbday}}{{$moneyMsg = print $moneyMsg $moneytogiveonbday $currency " For Your Bday**"}}{{ $increasemoney := dbIncr $user.ID $dbHandName $moneytogiveonbday }} {{sendMessageNoEscape nil $moneyMsg}} {{end}} {{sendMessageNoEscape nil $bdayMsg}} {{end}}
 {{else}}
 	{{if $isMod}}
 		{{if and (reFind `(?i)set` .Cmd) $isValidDate (not $error)}}
