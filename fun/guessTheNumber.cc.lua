749f1b4c4e498c67745e32f0031996dda0ea960d
DaviiiD1337 <68548032+DaviiiD1337@users.noreply.github.com>
2020-08-12 23:12:59 +0300

    Update guessTheNumber.cc.lua
    V2 💯

--- fun/guessTheNumber.cc.lua
+++ fun/guessTheNumber.cc.lua
@@ -1,36 +1,87 @@
 {{/*
 	This command is a game where users need to send numbers from 1 to 100. The winners win an amount of credits!
-
+ 
 	Recommended trigger: Regex trigger with trigger .*
+		
+	V2 by Dav!dﾉᵈᶻ#8302 (555791735607787580)
 */}}
+ 
 {{/* CONFIGURATION VALUES START */}}
-{{ $channel := replace-with-channel-id }} {{/* Channel ID where the game is played */}}
-{{ $prize := replace-with-prize }} {{/* The number of credits if the user won */}}
-{{ $db := "replace-with-db-name"}} {{/* The db name where the credits are stored  */}}
+{{ $channel := 722554898612355093 }} {{/* Channel ID where the game is played */}}
+{{ $prize := 400 }} {{/* The number of credits if the user won */}}
+{{ $db := "CREDITS"}} {{/* The db name where the credits are stored  */}}
+{{ $logs := 738718161645338694 }} {{/* Send the number in the logs channel, optional, set to "" if no logs channel */}}
+{{ $information := true }} {{/* true = hints activated | false = hints disabled */}}
+{{ $infoat := 5 }} {{/* When should hints be displayed? After how many fails */}}
 {{/* CONFIGURATION VALUES END */}}
-
+{{/* DONT TOUCH BELOW !! */}}
+ 
+ 
+{{/* Some variables */}}
+{{ $icon := "https://cdn.discordapp.com/attachments/741968239332163618/742027618907324436/warning.png" }}
+{{ if .Guild.Icon }}
+	{{ $icon = printf "https://cdn.discordapp.com/icons/%d/%s.webp" .Guild.ID .Guild.Icon }}
+{{ end }}
+{{ $embed := sdict }}
+{{ $col := sdict
+"finish" 6729778
+"start" 16752384
+"info" 37807 }}
+{{ $fields := cslice }}
+{{/* End of some variables :D */}}
+ 
 {{ if eq .Channel.ID $channel }}
+	{{ $nr := (toInt (dbGet 0 "NR").Value) }}
 	{{ if not .ExecData }}
 		{{ with reFindAllSubmatches `\d+` .Message.Content }}
-    			{{ if (eq (toInt (dbGet 0 "NR").Value) (toInt (index (index  . 0) 0))) }}
-				{{ dbDel 0 "NR" }} 
-    				{{ $r := dbIncr $.User.ID $db $prize }}
-   				{{ sendMessage nil (cembed 
-    				"title" "**We have a winner**" 
-    				"description" (print "Congrats " $.User.Username " you won!\nFor prize you won **" $prize "** credits !\nNow you have **" (toInt (dbGet $.User.ID $db).Value) "** credits.\nThe number was **" ( toInt (dbGet 0 "NR").Value) "** \nAnother number will be generated shortly !")
-    				"thumbnail" (sdict "url" "https://cdn.discordapp.com/attachments/656522874491895808/673899344033742848/lottery-png-4.png")
-    				"color" 34749 )}}
-				{{ execCC $.CCID $channel 10 (sdict "value" "true") }}
-			{{ end }}
-		{{ end }}
+    			{{ if (eq $nr (toInt (index (index  . 0) 0))) }}
+				    {{ dbDel 0 "NR" }} 
+    				    {{ $r := dbIncr $.User.ID $db $prize }}
+				    {{ $embed.Set "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "256")) }}
+				    {{ $embed.Set "color" ($col.Get "finish") }}
+				    {{ $fields = $fields.Append (sdict "name" "You won" "value" (print $prize " " $db) "inline" true) }}
+				    {{ $fields = $fields.Append (sdict "name" "The number was" "value" (print $nr) "inline" true) }}
+				    {{ $fields = $fields.Append (sdict "name" "Now you have" "value" (print $r " " $db) "inline" true) }}
+				    {{ $embed.Set "fields" $fields }}
+				    {{ dbDel 0 "hintsat" }}
+				    {{ execCC $.CCID $channel 10 "something" }}
+			{{ else if $information }}
+				    {{ $k := dbIncr 0 "hintsat" 1 }}
+				    {{ if eq (toInt $k) $infoat }}
+				    	{{ $hint1 := (sub $nr (randInt 1 20)) }}		
+					    {{ if le $hint1 1 }}
+						    {{ $hint1 = 1 }}
+						{{ end }}
+					{{ $hint2 := (add $nr (randInt 1 20)) }}
+					    {{ if ge $hint2 100 }}
+							{{ $hint2 = 100 }}
+						{{ end }}
+					{{ $fields = $fields.Append (sdict "name" "Hint" "value" (print "The number is between **" $hint1 "** and **" $hint2 "**")) }}
+					{{ $embed.Set "author" (sdict "name" $.User.Username "icon_url" ($.User.AvatarURL "256")) }}
+					{{ $embed.Set "fields" $fields }}
+					{{ $embed.Set "thumbnail" (sdict "url" "https://cdn.discordapp.com/attachments/741981782731391047/742021214544855040/idea.png") }}
+					{{ $embed.Set "color" ($col.Get "info") }}
+			    {{ end }}
+		    {{ end }}
+	{{ end }}
 	{{ else }}
+    {{ if not $information }}
+        {{ $infoat = "❌" }}
+    {{ end }}
 	{{ $x := randInt 100 }}
 	{{ dbSet 0 "NR" $x }}
-	{{ sendMessage nil (cembed 
-	"title" "**NEW NUMBER**" 
-	"description" (print "A new number has been generated!\nType numbers between **1** and **100**.\nIf you guess the number you win **" $prize "** credits!")
-	"thumbnail" (sdict "url" "https://cdn.discordapp.com/attachments/656522874491895808/673899344033742848/lottery-png-4.png")
-	"color" 14232643
-	"image" (sdict "url" "https://cdn.discordapp.com/attachments/695780710752976998/696349442303066212/slotmachine_2.gif")) }}
+	{{ $embed.Set "author" (sdict "name" .Guild.Name "icon_url" $icon) }}
+	{{ $embed.Set "description" "A new number generated! Type numbers from **1** to **100** to try your best :D" }}
+    	{{ $fields = $fields.Append (sdict "name" "Winnings" "value" (print $prize " " $db) "inline" true) }}
+	{{ $fields = $fields.Append (sdict "name" "Hints" "value" (print $information) "inline" true) }}
+	{{ $fields = $fields.Append (sdict "name" "Hints at" "value" (print $infoat) "inline" true) }}
+	{{ $embed.Set "color" ($col.Get "start") }}
+	{{ $embed.Set "fields" $fields }}
+		{{ if $logs }}
+			{{ sendMessage $logs (print "The number that generated is: **" $x "**") }}
+		{{ end }}
 	{{ end }}
+{{ end }}
+{{ if $embed }}
+	{{ sendMessage nil (cembed $embed) }}
 {{ end }}
