525438ae9871e9f7b8c6a4e6baf93276c2fb5dcc
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2020-11-19 13:10:02 +0530

    Update Interval.go

--- Bump CC/Interval.go
+++ Bump CC/Interval.go
@@ -1,11 +1,13 @@
 {{/* Trigger :- Minute Interval 
 	10min
 */}}
+
 {{/*Update the IDs*/}}
 {{$vcid := 774645677590446083}} {{/*Voice Channel which shows the Bump Time Remaining*/}}
 {{$channelid := 774658131204505610}} {{/*Channel where Bump Notifications need to appear*/}}
 
 {{/* Dont edit unless you know what you're doing. Better not edit, if you do, you need to change the Database and other things in the other CC's too*/}}
+
 {{if $db := dbGet 0 "cooldown"}}
 	{{editChannelName $vcid (print "Next Bump in " (($db.ExpiresAt.Sub currentTime).Round .TimeSecond))}}
 {{else}}
