1b9272a33e72569edb554174f1a947e75ceb8ed3
Joe <ravenclawjoe101325@gmail.com>
2020-11-23 12:55:22 -0800

    revert: fix: delete maze cc (#101)
    This reverts commit 8550cdfec9bb8e4fbc7c530345918f16fceb70cb.
    
    Closes #104

---
+++ fun/maze.cc.lua
@@ -0,0 +1,22 @@
+{{/*
+     Made by Crenshaw#7860
+	    This command sends a maze with an optional amount crossings/bridges. It also includes a link to a downloadable solution and .ExecData support. 
+     Usage: `-maze <cross amount>` <cross amount> is a optional argument on how many bridges/crossings you want
+
+ 	  Recommended Trigger: Command trigger with trigger `maze`
+*/}}
+
+{{/* No touchy touchy */}}
+{{ $seed := randInt 100000000 999999999 }}
+{{ $embed := sdict "thumbnail" (sdict "url" (.User.AvatarURL "128")) "title" (title "maze") "description" (joinStr "" "Requested by " .User.String " -") "image" (sdict "url" nil) "color" 123 }}
+{{ $crossings := 0 }}
+{{ if .CmdArgs }}
+	{{ $crossings = (index .CmdArgs 0) }}
+{{ else if .ExecData }}
+	{{$crossings = .ExecData }}
+{{ end }}
+{{ $maze := joinStr "" "http://maze5.de/cgi-bin/maze?sample=1&type=4&rows=12&columns=12&crossings=" $crossings "&seed=" $seed "&algorithm=backtracker&algorithm=0.5&foreground=%23ffffff&background=%2336393f&bordersize=16&cellsize=32&linewidth=2.5&format=png" }}
+{{ $embed.image.Set "url" $maze }}
+{{ $maze:= (joinStr "" $maze "&solution=true") }}
+{{ $embed.Set "description" (print $embed.description " [Solution](" $maze ")") }}
+{{ sendMessage nil (cembed $embed) }}
