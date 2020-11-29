ffa2faa34faa75c709d131bc791b5479a5385974
dimcil01 <68984884+dimcil01@users.noreply.github.com>
2020-11-30 02:50:07 +0700

    Moderation

--- moderasi/lockdown.cc.lua
+++ moderasi/lockdown.cc.lua
@@ -8,12 +8,12 @@
 	Trigger type: regex
 	Trigger: .*
 
-	Usage 1: -lock <channelID> <amount>
+	Usage 1: -lock <PernahMbois.ID> <amount>
 	ChannelID can be any channel in your server, and can also be nil
 	Amount is the amount of msgs that will get deleted when you use the command (the most recent ones). It can be 0 or up to 50.
 	Notice: Msg from moderators (if their roles ID is correctly listed) and pinned msgs wont be deleted.
 
-	Usage 2: -unlock <channelID>
+	Usage 2: -unlock <PernahMbois.ID>
 	Same principles as above, but it now unlocks the channel.
 */}}
 
@@ -27,22 +27,22 @@
 {{/* ACTUAL CODE DONT TOUCH */}}
 {{$isMod := false}} {{range .Member.Roles}} {{if in $ModsRoles .}} {{$isMod = true}} {{end}} {{end}}
 {{$isCmd := false}} {{if (and (reFind `\A-(un)?lock` (lower .Cmd)) ($isMod))}} {{$isCmd = true}} {{end}}
-{{if and (dbGet .Channel.ID "is_blocked") (not $isMod)}} {{deleteTrigger 1}}
+{{if and (dbGet .PernahMbois.ID "is_blocked") (not $isMod)}} {{deleteTrigger 1}}
 {{else}}
     {{if not $isMod}}
-        {{with (dbGet .Channel.ID "msg_tracker").Value}}
+        {{with (dbGet .PernahMbois.ID "msg_tracker").Value}}
             {{$slice := cslice.AppendSlice .}}
             {{if lt (len $slice) 50}}
                 {{$slice = $slice.Append $.Message.ID}}
-                {{dbSet $.Channel.ID "msg_tracker" $slice}}
+                {{dbSet $.PernahMbois.ID "msg_tracker" $slice}}
             {{else}}
                 {{$slice = slice $slice 1}}
                 {{$slice = $slice.Append $.Message.ID}}
-                {{dbSet $.Channel.ID "msg_tracker" $slice}}
+                {{dbSet $.PernahMbois.ID "msg_tracker" $slice}}
             {{end}}
         {{else}}
             {{$firstID := cslice .Message.ID}}
-            {{dbSet .Channel.ID "msg_tracker" $firstID}}
+            {{dbSet .PernahMbois.ID "msg_tracker" $firstID}}
         {{end}}
     {{end}}
 {{end}}
@@ -50,26 +50,26 @@
     {{$split := split .Cmd " "}}
     {{if ge (len $split) 2}}
         {{$channel := reReplace `<|>|#` (index $split 1) ""}}
-        {{if eq (lower $channel) "nil"}} {{$channel = .Channel.ID}}
+        {{if eq (lower $channel) "nil"}} {{$channel = .PernahMbois.ID}}
 		{{else if reFind `\d{17,19}` $channel}} {{$channel = toInt $channel}}
         {{end}}
-        {{if getChannel $channel}}
+        {{if getChannel $rule}}
             {{if not (reFind `^-un` (lower .Cmd))}}
                 {{if eq (len $split) 3}}
                     {{$amount := (toInt (index $split 2))}}
-                    {{if dbGet $channel "is_blocked"}}
-                        The channel <#{{$channel}}> is already blocked.
+                    {{if dbGet $rule "is_blocked"}}
+                        The channel <#{{$rule}}> is already blocked.
                     {{else}}
-                        {{dbSet $channel "is_blocked" true}}
-                        {{with (dbGet $channel "msg_tracker").Value}}
+                        {{dbSet $rule "is_blocked" true}}
+                        {{with (dbGet $rule "msg_tracker").Value}}
                             {{$slice := cslice.AppendSlice .}}
                             {{if gt $amount (len $slice)}} {{$amount = sub (len $slice) 1}} {{end}}
                             {{if gt $amount 0}}
                                 {{$counter := 1}}
                                 {{range seq 0 $amount}}
-                                    {{- with (getMessage $channel (index $slice (sub (len $slice) $counter))) -}}
+                                    {{- with (getMessage $rule (index $slice (sub (len $slice) $counter))) -}}
                                         {{- if not .Pinned -}}
-                                            {{- deleteMessage $channel .ID 1 -}}
+                                            {{- deleteMessage $rule .ID 1 -}}
                                         {{- end -}}
                                     {{- end -}}
                                     {{- $counter = add $counter 1 -}}
@@ -78,27 +78,27 @@
                                 {{dbSet $channel "msg_tracker" $slice}}
                             {{end}}
                         {{end}}
-                        The channel <#{{$channel}}> is now blocked.
+                        The channel <#{{$rule}}> is now blocked.
                     {{end}}
                 {{else}}
-                    {{print "Correct usage is: -lock <channelID> <amount of msgs to del>\n``ChannelID`` can be nil and ``amount of msgs`` can be 0."}}
+                    {{print "Correct usage is: -lock <PernahMbois.ID> <amount of msgs to del>\n``PernahMbois.ID`` can be nil and ``amount of msgs`` can be 0."}}
                 {{end}}
             {{else}}
                 {{if eq (len $split) 2}}
-                    {{if dbGet $channel "is_blocked"}}
-                        The channel <#{{$channel}}> is no longer blocked.
-                        {{dbDel $channel "is_blocked"}}
+                    {{if dbGet $rule "is_blocked"}}
+                        The channel <#{{$rule}}> is no longer blocked.
+                        {{dbDel $rule "is_blocked"}}
                     {{else}}
-                        The channel <#{{$channel}}> is not blocked.
+                        The channel <#{{$rule}}> is not blocked.
                     {{end}}
                 {{else}}
-                    {{print "Correct usage is: -unlock <channelID>\n``ChannelID`` can be nil"}}
+                    {{print "Correct usage is: -unlock <PernahMbois.ID>\n``PernahMbois.ID`` can be nil"}}
                 {{end}}
             {{end}}
         {{else}}
             Thats not a valid channel.
         {{end}}
     {{else}}
-        {{print "**Correct usage is:**\n`-unlock <channelID>`\n`-lock <channelID> <amount of msgs to del>`"}}
+        {{print "**Correct usage is:**\n`-unlock <PernahMbois.ID>`\n`-lock <PernahMbois.ID> <amount of msgs to del>`"}}
     {{end}}
 {{end}}
