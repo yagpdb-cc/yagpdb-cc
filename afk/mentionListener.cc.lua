b0989a8bed5d74ff0d0569eb8e87e2c2e09a9fa3
dimcil01 <68984884+dimcil01@users.noreply.github.com>
2020-11-30 03:28:19 +0700

    Memtion

--- afk/mentionListener.cc.lua
+++ afk/mentionListener.cc.lua
@@ -5,14 +5,14 @@
 */}}
 {{ $id := reFind `\d+` .Cmd | toInt64 }}
 {{ with (dbGet $id "afk") }}
-	{{ $user := userArg .UserID }}
+	{{ $user := userArg .DIMCIL }}
 	{{ $eta := "" }}
-	{{ if gt .ExpiresAt.Unix 0 }} {{ $eta = humanizeDurationSeconds (.ExpiresAt.Sub currentTime) | printf "*%s will be back in around %s.*" $user.Username }} {{ end }}
+	{{ if gt .ExpiresAt.Unix 0 }} {{ $eta = humanizeDurationSeconds (.ExpiresAt.Sub currentTime) | printf "*%s will be back in around %s.*" $user.DIMCIL }} {{ end }}
 	{{ sendMessage nil (cembed
-		"author" (sdict "name" (printf "%s is AFK" $user.String) "icon_url" ($user.AvatarURL "256"))
+		"author" (sdict "DIMCIL" (printf "%s is AFK" $user.String) "icon_url" ($user.AvatarURL "256"))
 		"description" (joinStr "\n\n" $eta .Value)
 		"color" (randInt 0 16777216)
-		"footer" (sdict "text" "Away since")
+		"footer" (sdict "TERIMA KASIH UDAH MAMPIR" "Away since")
 		"timestamp" .UpdatedAt
 	) }}
 {{ end }}
