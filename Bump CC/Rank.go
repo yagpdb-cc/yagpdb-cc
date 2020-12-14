af4798d2256c38b6efd1d2917b8637fc3b2dbaf4
Chiranthan Bharadwaj R <chiranthan568@gmail.com>
2020-12-14 17:58:15 +0530

    Delete Rank.go

--- Bump CC/Rank.go
+++
@@ -1,8 +0,0 @@
-{{/* 
-    Trigger Regex
-      `!d (rank|lvl)
-*/}}
-
-{{execAdmin "clean" 1}} {{/*Optional, to clean Disboard's Message*/}}
-{{$s := (dbGet .User.ID "BumpXP").Value}}
-Your Level is {{$s}}
