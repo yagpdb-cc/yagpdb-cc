4ba82645a32c49cc16c51d3ef0ce621fc89bbd70
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-07-11 16:30:42 -0700

    fix add newline script

--- bin/add-newlines.sh
+++ bin/add-newlines.sh
@@ -1,4 +1,7 @@
+shopt -s globstar
+
 for filename in ../src/**/*.go.tmpl; do
+	echo "Adding EOF newline for $filename"
 	sed -i -e '$a\' $filename
 done
-echo 'Done processing files'
+echo 'Added EOF newlines in all custom command files'
