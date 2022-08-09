293268645c297dd1c2a0de9c634b8dd088d4e1b7
Joe L <56809242+jo3-l@users.noreply.github.com>
2022-08-09 12:34:17 -0700

    chore(website): update docusaurus (#282)
    * chore(website): upgrade docusaurus
    
    * chore(website): check in lockfile
    
    This is a generally accepted best practice for ensuring deterministic builds.
    
    * style(*): run prettier on codebase
    
    * ci(github): use Node v16
    
    * refactor(website): make spaces->tabs workaround more robust
    
    Co-authored-by: jo3-l <jo3-l@users.noreply.github.com>

--- website/src/theme/prism-include-languages.js
+++ website/src/theme/prism-include-languages.js
@@ -1,24 +1,20 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import siteConfig from '@generated/docusaurus.config';
-
-const prismIncludeLanguages = (PrismObject) => {
-	if (ExecutionEnvironment.canUseDOM) {
-		const {
-			themeConfig: { prism: { additionalLanguages = [] } = {} },
-		} = siteConfig;
-		window.Prism = PrismObject;
-		additionalLanguages.forEach((lang) => {
-			require(`prismjs/components/prism-${lang}`); // eslint-disable-line
-		});
-		require('../custom-languages/prism-gotmpl');
-		delete window.Prism;
-	}
-};
-
-export default prismIncludeLanguages;
+export default function prismIncludeLanguages(PrismObject) {
+	const {
+		themeConfig: { prism },
+	} = siteConfig;
+	const { additionalLanguages } = prism;
+	// Prism components work on the Prism instance on the window, while prism-
+	// react-renderer uses its own Prism instance. We temporarily mount the
+	// instance onto window, import components to enhance it, then remove it to
+	// avoid polluting global namespace.
+	// You can mutate PrismObject: registering plugins, deleting languages... As
+	// long as you don't re-assign it
+	globalThis.Prism = PrismObject;
+	additionalLanguages.forEach((lang) => {
+		// eslint-disable-next-line global-require, import/no-dynamic-require
+		require(`prismjs/components/prism-${lang}`);
+	});
+	require('../custom-languages/prism-gotmpl');
+	delete globalThis.Prism;
+}
