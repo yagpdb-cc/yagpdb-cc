6ca6af6fad8049f4791f59a9d70d0a105697b422
Joe L <56809242+jo3-l@users.noreply.github.com>
2022-08-09 14:18:30 -0700

    feat(website): improve syntax highlighting (#284)
    * chore(website): update prism language definition
    
    * fix(website): use better highlighted code color
    
    * chore(bin): add generator for syntax highlighting func list
    
    * fix(website): update syntax highlighting func list
    
    * ci(github): add job to regenerate funcs
    
    * ci(github): fix pull request description

--- website/src/custom-languages/prism-gotmpl/index.js
+++ website/src/custom-languages/prism-gotmpl/index.js
@@ -3,15 +3,26 @@ import { funcs } from './funcs';
 Prism.languages.gotmpl = {
 	comment: /{{\/\*.+?\*\/}}/s,
 	string: {
-		pattern: /(["`])(?:\\[\s\S]|(?!\1)[^\\])*\1/,
+		pattern: /(^|[^\\])"(?:\\.|[^"\\\r\n])*"|`[^`]*`/,
+		lookbehind: true,
 		greedy: true,
 	},
-	keyword: /\b(?:if|range|template|define|block|with|else)\b/,
+	keyword: /\b(?:if|else|end|range|while|break|continue|try|catch|return|template|define|block|with)\b/,
 	boolean: /\b(?:nil|true|false)\b/,
-	number: /(?:\b0x[a-f\d]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[-+]?\d+)?)i?/i,
-	builtin: /\b(?:call|html|index|slice|js|len|print(?:f|ln)|urlquery)\b/,
+	number: [
+		// binary and octal integers
+		/\b0(?:b[01_]+|o[0-7_]+)i?\b/i,
+		// hexadecimal integers and floats
+		/\b0x(?:[a-f\d_]+(?:\.[a-f\d_]*)?|\.[a-f\d_]+)(?:p[+-]?\d+(?:_\d+)*)?i?(?!\w)/i,
+		// decimal integers and floats
+		/(?:\b\d[\d_]*(?:\.[\d_]*)?|\B\.\d[\d_]*)(?:e[+-]?[\d_]+)?i?(?!\w)/i,
+	],
+	builtin: /\b(?:call|html|index|slice|js|len|print(?:f|ln)|urlquery|execTemplate)\b/,
 	operator: /\b(?:eq|ne|lt|le|gt|ge|not|and|or)\b/,
 	variable: /\$(?:[A-Za-z_]\w*)?\b/,
 	function: new RegExp(String.raw`\b(?:${funcs.join('|')})\b`),
-	char: /'.'/,
+	char: {
+		pattern: /'(?:\\.|[^'\\\r\n]){0,10}'/,
+		greedy: true,
+	},
 };
