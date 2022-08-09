5b6011d55a72d275b39fa5890d8e37bf64829b09
jo3-l <jo3-l@users.noreply.github.com>
2022-08-09 06:10:07 +0000

    style(*): run prettier on codebase

--- website/src/theme/CodeBlock/index.js
+++ website/src/theme/CodeBlock/index.js
@@ -1,4 +1,4 @@
-import React, {isValidElement} from 'react';
+import React, { isValidElement } from 'react';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import ElementContent from '@theme/CodeBlock/Content/Element';
 import StringContent from '@theme/CodeBlock/Content/String';
@@ -9,24 +9,23 @@ import StringContent from '@theme/CodeBlock/Content/String';
  * together.
  */
 function maybeStringifyChildren(children) {
-  if (React.Children.toArray(children).some((el) => isValidElement(el))) {
-    return children;
-  }
-  // The children is now guaranteed to be one/more plain strings
-  return Array.isArray(children) ? children.join('') : children;
+	if (React.Children.toArray(children).some((el) => isValidElement(el))) {
+		return children;
+	}
+	// The children is now guaranteed to be one/more plain strings
+	return Array.isArray(children) ? children.join('') : children;
 }
-export default function CodeBlock({children: rawChildren, ...props}) {
-  // The Prism theme on SSR is always the default theme but the site theme can
-  // be in a different mode. React hydration doesn't update DOM styles that come
-  // from SSR. Hence force a re-render after mounting to apply the current
-  // relevant styles.
-  const isBrowser = useIsBrowser();
-  const children = maybeStringifyChildren(rawChildren);
-  const CodeBlockComp =
-    typeof children === 'string' ? StringContent : ElementContent;
-  return (
-    <CodeBlockComp key={String(isBrowser)} {...props}>
-      {children}
-    </CodeBlockComp>
-  );
+export default function CodeBlock({ children: rawChildren, ...props }) {
+	// The Prism theme on SSR is always the default theme but the site theme can
+	// be in a different mode. React hydration doesn't update DOM styles that come
+	// from SSR. Hence force a re-render after mounting to apply the current
+	// relevant styles.
+	const isBrowser = useIsBrowser();
+	const children = maybeStringifyChildren(rawChildren);
+	const CodeBlockComp = typeof children === 'string' ? StringContent : ElementContent;
+	return (
+		<CodeBlockComp key={String(isBrowser)} {...props}>
+			{children}
+		</CodeBlockComp>
+	);
 }
